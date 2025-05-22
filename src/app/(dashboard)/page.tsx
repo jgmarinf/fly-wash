204084024f8ced1d1ff1548d8921b416243d0332
jgmarinf <jgmarinf93@gmail.com>
2025-05-22 13:10:12 -0500

    feat(dashboard): remove ButtonCreateMachine from main page
    The ButtonCreateMachine component is removed from the dashboard page to streamline the UI, as its functionality will be relocated or handled differently.

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,8 @@
 import MachineGrid from "@/components/machine/MachineGrid"
-import ButtonCreateMachine from "@/components/ui/ButtonCreateMachine"
 
 const page = () => {
   return (
     <div className="bg-blue-950 p-5 flex flex-col gap-2 w-full min-h-screen overflow-auto">
-      <ButtonCreateMachine/>
       <MachineGrid />
     </div>
   )
