1f45ddcafcf296f65917019740697ad85adde3aa
jgmarinf <jgmarinf93@gmail.com>
2025-06-12 13:58:02 -0500

    chore(components): export new UI components in index file
    Add exports for ButtonSendMoney, ButtonDashboard and ButtonUpDate
    to components index file for centralized import convenience.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export * from "./machine/Machine";
 export * from "./machine/MachineGrid";
-export * from "./ui/ButtonLogout"
+export * from "./ui/ButtonLogout";
+export * from "./ui/ButtonSendMoney";
+export * from "./ui/ButtonDashboard";
+export * from "./ui/ButtonUpDate";
